4ad42e04a85dc61322b0b187bb008824d37fe6e6
alexey_t <alexterleev682004@gmail.com>
2023-09-28 17:55:17 +0300

    redisign

--- PBZ/lab2/app/exhibitions/exhibitions/urls.py
+++ PBZ/lab2/app/exhibitions/exhibitions/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
 
 from django.views.generic import TemplateView
 from main import views
@@ -23,10 +24,8 @@ from main import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path("", views.index),
-
-    path("test/", views.exhibition_halls),
-
+    path('', lambda request: redirect('exhibits/', permanent=False)),
+    path("exhibits/", views.exhibits),
+    path("exhibition_halls/", views.exhibition_halls),
     path("exhibitions/", views.exhibitions)
 ]
--- PBZ/lab2/app/exhibitions/main/views.py
+++ PBZ/lab2/app/exhibitions/main/views.py
@@ -1,65 +1,68 @@
+from typing import Any
 from django.shortcuts import render
+from django.http import HttpRequest
 from .models import Exhibits, ExhibitionHalls, Exhibitions
 from .forms import ExebitionForm, CityForm
 from datetime import date
 
-# Create your views here.
 
 
-def index(request):
-    
-    filter_id = request.POST.get("exebition") if request.method == "POST" else 1
-    exebition_form = ExebitionForm(initial={"exebition": filter_id})
+def get_filter(request: HttpRequest, filter_name: str, default_value):
+    return request.POST.get(filter_name) if request.method == "POST" else default_value
 
-    exhibits = Exhibits.objects.filter(exhibition_id=filter_id).select_related("artist_id")
-    data = []
-    for exhibit in exhibits:
-        artist_birth_date = exhibit.artist_id.birth_date
-        today = date.today()
-        data.append(
-            {
-                "exhibit_name": exhibit.name,
-                "artist_name": exhibit.artist_id.name,
-                "artist_age": today.year - artist_birth_date.year - ((today.month, today.day) < (artist_birth_date.month, artist_birth_date.day)),
-                "exhibit_creation_date": exhibit.date
-            }
-        )
-    return render(request, "index.html", context={"form": exebition_form, "data": data})
+
+def get_data_exhibit(exhibit):
+    artist_birth_date = exhibit.artist_id.birth_date
+    today = date.today()
+    age = today.year - artist_birth_date.year - ((today.month, today.day) < (artist_birth_date.month, artist_birth_date.day))
+    return {
+        "exhibit_name": exhibit.name,
+        "artist_name": exhibit.artist_id.name,
+        "artist_age": age,
+        "exhibit_creation_date": exhibit.date
+    }
+
+
+def get_data_exhibition_hall(exhibition_hall):
+    return {
+        "date": None,
+        "name": exhibition_hall.name,
+        "address": exhibition_hall.adress,
+        "space": exhibition_hall.space,
+        "owner": exhibition_hall.owner_id.name,
+    }
+
+
+def get_data_exhibition(exhibition):
+    return {
+        "date": exhibition.date,
+        "name": exhibition.name,
+        "hall_address": exhibition.exhibition_hall_id.adress,
+    }
+
+
+def exhibits(request):
+    filter_name, default_value = "exebition", 1
+    filter = get_filter(request, filter_name, default_value)
+    form = ExebitionForm(initial={filter_name: filter})
+    rows = Exhibits.objects.filter(exhibition_id=filter).select_related("artist_id")
+    data = [get_data_exhibit(row) for row in rows]
+    return render(request, "exhibits.html", context={"form": form, "data": data})
     
     
 def exhibition_halls(request):
-    city_name = request.POST.get("city") if request.method == "POST" else "Минск"
-    city_form = CityForm(initial={"city": city_name})
-
-    exhibition_halls = ExhibitionHalls.objects.select_related().filter(adress__icontains=city_name)
-    data = []
-    for exhibition_hall in exhibition_halls:
-        data.append(
-            {
-                "date": None,
-                "name": exhibition_hall.name,
-                "address": exhibition_hall.adress,
-                "space": exhibition_hall.space,
-                "owner": exhibition_hall.owner_id.name,
-            }
-        )
-
-    return render(request, "test.html", context={"form": city_form, "data": data})
+    filter_name, default_value = "city", "Minsk"
+    filter = get_filter(request, filter_name, default_value)
+    form = CityForm(initial={filter_name: filter})
+    rows = ExhibitionHalls.objects.select_related().filter(adress__icontains=filter)
+    data = [get_data_exhibition_hall(row) for row in rows]
+    return render(request, "exhibition_halls.html", context={"form": form, "data": data})
 
 
 def exhibitions(request):
-    city_name = request.POST.get("city") if request.method == "POST" else "Минск"
-    city_form = CityForm(initial={"city": city_name})
-
-    exhibitions = Exhibitions.objects.select_related().filter(exhibition_hall_id_id__adress__icontains=city_name)
-    data = []
-    for exhibition in exhibitions:
-        data.append(
-            {
-                "date": exhibition.date,
-                "name": exhibition.name,
-                "hall_address": exhibition.exhibition_hall_id.adress,
-            }
-        )
-
-    return render(request, "exhibitions.html", context={"form": city_form, "data": data})
+    filter_name, default_value = "city", "Minsk"
+    filter = get_filter(request, filter_name, default_value)
+    form = CityForm(initial={filter_name: filter})
+    rows = Exhibitions.objects.select_related().filter(exhibition_hall_id_id__adress__icontains=filter)
+    data = [get_data_exhibition(row) for row in rows]
+    return render(request, "exhibitions.html", context={"form": form, "data": data})
